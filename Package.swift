471750648451b0d9f2f8f5ede91c1bda80620eed
Max Braver <max@6over6.com>
2023-02-01 12:02:52 +0200

    New Package content from Eugene

--- Package.swift
+++ Package.swift
@@ -1,23 +1,37 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
- 
+
 let package = Package(
-    name: "PHISDK",
-    platforms: [
-        .iOS(.v11)
-    ],
+    name: "PHITestPackage",
+    defaultLocalization: "en-US",
+    platforms: [.iOS(.v15)],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PHILunaSDK",
-            targets: ["PHILunaSDK"]),
+            name: "PHITestPackage",
+            targets: ["PHITestPackage"]),
     ],
     dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/mixpanel/mixpanel-swift",
+            "3.2.0" ..< "4.0.0"
+        )
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "PHITestPackage",
+            dependencies: [.product(name: "Mixpanel", package: "mixpanel-swift"),
+                           .target(name: "PHI")]
+        ),
         .binaryTarget(
-            name: "PHILunaSDK",
-            url: "https://glasseson.jfrog.io/ui/native/swift-virtual/PHILunaSDK.xcframework.zip",
-            checksum: "c9b0f682385c6e66a65901a1f1d40c9f0e88d3eeb0c752046cede42ab2420b5d"
-        )
+            name: "PHI",
+            url: "https://fileserver.test.glasseson.com/PHILunaSDK.xcframework.zip",
+            checksum: "2268025e8c2c8613f9713bdd3132ba89c394fcc12ce95f3d164181f44bd14c9c")
     ]
 )
