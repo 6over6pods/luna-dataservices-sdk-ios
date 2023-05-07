96f6b56a8e02cba2d228d68a9f40d164757d5f12
Eugene Gordenstein <eugene@6over6.com>
2023-05-07 14:46:40 +0300

    [release-7.0.1] - Framework update

--- Package.swift
+++ Package.swift
@@ -17,18 +17,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/mixpanel/mixpanel-swift",
-            "3.2.0" ..< "4.0.0"
-        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PHIWrapper",
-            dependencies: [.product(name: "Mixpanel", package: "mixpanel-swift"),
-                           .target(name: "PHISDK")]
+            dependencies: [.target(name: "PHISDK")]
         ),
         .binaryTarget(
             name: "PHISDK",
