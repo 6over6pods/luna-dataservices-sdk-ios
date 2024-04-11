ce47f371e1eac5706e5a3dc9408a99ebd5c0459b
deployer-6over6 <deployer-6over6@6over6.com>
2024-04-11 14:46:41 +0000

    deployer-6over6: opening branch with changes in Package.swift

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PHISDK",
-            url: "https://glasseson.jfrog.io/artifactory/luna-dataservices-sdk-ios-local/8.3.0/PHISDK.xcframework.zip",
-            checksum: "b41c3cb8411f49a9aea67edf009ed79124d38664401ede8fb6f8707ec18c8b02"
+            url: "https://glasseson.jfrog.io/artifactory/luna-dataservices-sdk-ios-local/8.3.1-39/PHISDK.xcframework.zip",
+            checksum: "1b0baabc9e5ecb27c7444c5c92693f001ea1aa561efd7c03680a991306c112b7"
         )
     ]
 )
